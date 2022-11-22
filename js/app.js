afa32749767361fea6d6c40f56282238a3c6abd9
superigrooper <superigrooper@gmail.com>
2022-11-22 21:02:06 +0300

    fix: fixed mobile menu script

--- js/app.js
+++ js/app.js
@@ -21,6 +21,14 @@ $(document).ready(function() {
     $('body').toggleClass('stop-scroll');
   })
 
+  $('.header__nav-link').each(function(index, elem) {
+    $(this).click(function () {
+      $('.header__burger').toggleClass('header__burger--active');
+      $('.header__nav').toggleClass('header__nav--mobile');
+      $('body').toggleClass('stop-scroll');
+    })
+  })
+
   // Кнопка "наверх"
   $(".to-up").bind('click', function(e){
     e.preventDefault();
