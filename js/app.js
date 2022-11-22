cb3f2d461b6c719c3a8b1d36b3f2e5e7b149ca91
superigrooper <superigrooper@gmail.com>
2022-11-22 20:17:47 +0300

    edit: the basic functionality of the site is ready

--- js/app.js
+++ js/app.js
@@ -1,14 +1,37 @@
 $(document).ready(function() {
-  var $window = $(window);
-  var $header = $('.header');
-  var $height = document.documentElement.clientHeight;
+
+  // Прокрутка окна и фиксация меню
+  const $window = $(window);
+  const $header = $('.header');
+  const $height = document.documentElement.clientHeight;
 
   $window.on('scroll', function() {
-    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     if (scrollTop > $height) {
       $header.addClass('header--fixed');
-    } else {     
+    } else {
       $header.removeClass('header--fixed');
     }
   });
+
+  // Бургер и мобильное меню
+  $('.header__burger').click(function () {
+    $(this).toggleClass('header__burger--active');
+    $('.header__nav').toggleClass('header__nav--mobile');
+    $('body').toggleClass('stop-scroll');
+  })
+
+  // Кнопка "наверх"
+  $(".to-up").bind('click', function(e){
+    e.preventDefault();
+    $('body,html').animate({scrollTop: 0}, 600);
+  });
+
+  // Дата в футере
+  $('.footer__now').html(new Date().getFullYear());
+});
+
+// Плагин fancybox
+Fancybox.bind("#grouped-images img", {
+  groupAll: true,
 });
